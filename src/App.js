bc2070c9aed868c830b1cd798ab625d738c8d486
JordanDyck <jordandyck777@hotmail.com>
2023-04-17 11:22:55 -0600

    created basic calc funtions. everything works ok.

--- src/App.js
+++ src/App.js
@@ -16,25 +16,45 @@ const timeOptions = [
 ]
 
 function App() {
-  const [estimate, setEstimate] = useState({speed: "", size: ""})
+  const [download, setDownload] = useState({speed: 0, size: 0})
+  const [estimate, setEstimate] = useState()
+  const [downloadTime, setDownloadTime] = useState({time: "Hours"})
+  const [downloadType, setDownloadType] = useState({speed: "MB", size: "GB"})
 
-  // stores the size
-  const handleSize = (e) => {
-    setEstimate((curr) => ({
-      ...curr,
-      size: e.target.value,
-    }))
+  const calcSpeed = (speed) => {
+    return (
+      speed *
+      (downloadType.speed === "MB" ? 1024 : 1) *
+      (downloadType.speed === "GB" ? 1024 * 1024 : 1)
+    )
   }
 
-  //stores the speed
-  const handleSpeed = (e) => {
-    setEstimate((curr) => ({
-      ...curr,
-      speed: e.target.value,
-    }))
+  const calcSize = (size) => {
+    return (
+      size *
+      (downloadType.size === "MB" ? 1024 : 1) *
+      (downloadType.size === "GB" ? 1024 * 1024 : 1)
+    )
   }
 
-  // console.log("size:", estimate.size, "speed:", estimate.speed)
+  const timeLength = () => {
+    return calcSize(download.size) / calcSpeed(download.speed)
+  }
+
+  const calcTime = (time) => {
+    return (
+      time /
+      (downloadTime.time === "Mins" ? 60 : 1) /
+      (downloadTime.time === "Hours" ? 60 * 60 : 1) /
+      (downloadTime.time === "Days" ? 60 * 60 * 24 : 1)
+    )
+  }
+
+  // total time to download.
+  const calculate = () => {
+    const time = calcTime(timeLength()).toFixed(2)
+    setEstimate(time)
+  }
   return (
     <div className="wrapper">
       <header className="title"> How Long To Download</header>
@@ -42,13 +62,25 @@ function App() {
         <input
           className="size-input"
           type="number"
-          placeholder="Download Size"
+          placeholder="Download Size..."
+          onChange={(e) =>
+            setDownload((curr) => ({
+              ...curr,
+              size: e.target.value,
+            }))
+          }
         />
         <Select
           className="option"
           options={speedOptions}
           defaultValue={speedOptions[0]}
           isSearchable={false}
+          onChange={(e) =>
+            setDownloadType((curr) => ({
+              ...curr,
+              size: e.value,
+            }))
+          }
         />
       </div>
 
@@ -57,27 +89,45 @@ function App() {
           className="speed-input"
           type="number"
           placeholder="Download speed..."
-          onChange={handleSpeed}
+          onChange={(e) =>
+            setDownload((curr) => ({
+              ...curr,
+              speed: e.target.value,
+            }))
+          }
         />
         <Select
           className="option"
           options={speedOptions}
           defaultValue={speedOptions[1]}
           isSearchable={false}
+          onChange={(e) =>
+            setDownloadType((curr) => ({
+              ...curr,
+              speed: e.value,
+            }))
+          }
         />
       </div>
 
       <div className="time-container">
         <div className="result">
-          <label className="time-text">placeholder: </label>
+          <label className="time-text">{estimate}</label>
         </div>
         <Select
           className="option"
           options={timeOptions}
-          defaultValue={timeOptions[0]}
+          defaultValue={timeOptions[2]}
           isSearchable={false}
+          onChange={(e) =>
+            setDownloadTime((curr) => ({
+              ...curr,
+              time: e.value,
+            }))
+          }
         />
       </div>
+      <button onClick={() => calculate()}>calculate</button>
     </div>
   )
 }
